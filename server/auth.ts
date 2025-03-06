1153d0a04716ba5a20452dacf2c6285d3d1ba9b2
Abigail Chou <abbychou@mit.edu>
2025-03-06 11:37:53 -0500

    moved client id into process.env on the backend

--- server/auth.ts
+++ server/auth.ts
@@ -1,3 +1,4 @@
+import dotenv from "dotenv";
 import { NextFunction, Request, Response } from "express";
 import { OAuth2Client, TokenPayload } from "google-auth-library";
 
@@ -6,16 +7,14 @@ import socketManager from "./server-socket";
 import { IGoogleUser, ISession, IUser } from "./types/types";
 
 // create a new OAuth client used to verify google sign-in
-const CLIENT_ID =
-  "1077700528117-f59tr015sdectfbjnd9b6hqguqfmb5bi.apps.googleusercontent.com";
-const client = new OAuth2Client(CLIENT_ID);
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 // accepts a login token from the frontend, and verifies that it's legit
 function verify(token: string): Promise<TokenPayload | undefined> {
   return client
     .verifyIdToken({
       idToken: token,
-      audience: CLIENT_ID,
+      audience: process.env.GOOGLE_CLIENT_ID,
     })
     .then((ticket) => ticket.getPayload());
 }
