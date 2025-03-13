dee8c32767071e478b4c4e0b656f43c99b20ac4f
Abigail Chou <abbychou@mit.edu>
2025-03-13 11:48:37 -0400

    beginnings of drawing logic

--- client/src/draw.ts
+++ client/src/draw.ts
@@ -1,33 +1,42 @@
-/* all d\rawing-related functions and utilities */
-import { Canvas, TEvent } from "fabric";
+/* all drawing-related functions and utilities */
+import { Canvas, TPointerEventInfo, Circle } from "fabric";
 
+// Define the Mode object
 export const Mode = Object.freeze({
     ADD_LINE: "add_line",
 });
 export type ModeType = typeof Mode[keyof typeof Mode];
 
+// Function to add listeners for a specific mode
 export const addListenersForMode = (canvasObj: Canvas, mode: ModeType) => {
-    const adderFunc = activateModeFuncs[mode]
+    const adderFunc = activateModeFuncs[mode];
     adderFunc(canvasObj);
 };
 
+// Function to remove listeners for a specific mode
 export const removeListenersForMode = (canvasObj: Canvas, mode: ModeType) => {
-    const removerFunc = deactivateModeFuncs[mode]
+    const removerFunc = deactivateModeFuncs[mode];
     removerFunc(canvasObj);
 };
 
 const activateModeFuncs = {
     'add_line': (canvasObj: Canvas) => {
-        canvasObj.on("mouse:down", startDrawingLine)
+        const startDrawingLineWithCanvas = startDrawingLine(canvasObj); // Use closure to hold on to canvasObj
+        canvasObj.on("mouse:down", startDrawingLineWithCanvas);
     },
-}
+};
 
 const deactivateModeFuncs = {
     'add_line': (canvasObj: Canvas) => {
-        canvasObj.off("mouse:down", startDrawingLine)
+        const startDrawingLineWithCanvas = startDrawingLine(canvasObj);  // Use closure to hold on to canvasObj
+        canvasObj.off("mouse:down", startDrawingLineWithCanvas);
     },
-}
+};
 
-const startDrawingLine = (e: TEvent) => {
-    // TODO: implement
-}
+// Function to start drawing a line
+const startDrawingLine = (canvasObj: Canvas) => {
+    return (eventObj: TPointerEventInfo) => {
+        console.log(eventObj.scenePoint.x);
+        // const point = Circle();
+    };
+};
