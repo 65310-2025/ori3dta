d39056a984dd02a45cbca33b1fb131968030a709
kennyzzhang <kzzhang@mit.edu>
2025-04-29 11:58:03 -0400

    Add main

---
+++ fold_algos/src/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+#include <simdjson.h>
+
+#include <fold.h>
+#include <layer_solver.h>
+
+using namespace simdjson;
+using namespace ori3dta;
+
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    std::clog << "Usage: " << argv[0] << " <filename>.fold" << std::endl;
+    return 1;
+  }
+
+  padded_string json = padded_string::load(argv[1]);
+  ondemand::parser parser;
+
+  ondemand::document doc;
+  parser.iterate(json).get(doc);
+
+  ori3dta::FOLD fold;
+  doc.get(fold);
+  fold.compute_edges_faces_from_faces_edges();
+
+  LayerSolver solver(fold);
+
+  solver.solver.verbosity = -1;
+  std::cout << "Solver out: " << solver.solver.solve() << std::endl;
+  for (const auto& [k, v] : solver.overlaps_var) {
+    const auto& [f1, f2] = k;
+    std::cout << f1 << ", " << f2 << ": " << Glucose::toInt(solver.solver.value(v)) << std::endl;
+  }
+  return 0;
+}
