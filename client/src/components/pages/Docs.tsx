04d27f23d049d92905a26770f189e516a497f72c
theplantpsychologist@gmail.com <theplantpsychologist>
2025-05-07 13:50:01 -0400

    docs outline

--- client/src/components/pages/Docs.tsx
+++ client/src/components/pages/Docs.tsx
@@ -8,7 +8,28 @@ const Docs: React.FC = () => {
     <div>
       <Navbar />
       <div className="Home">
-        <div className="Docs"></div>
+        <div className="Docs">
+          <div className="Wrapper">
+            <div className="Page">
+              <h1>Documentation</h1>
+              <h2>Overview</h2>
+              <p>
+                who what why how. core features
+              </p>
+              <h2>Getting started</h2>
+              <p>
+                make a new design, basic tools
+              </p>
+              <h2>Tools reference</h2>
+              <p>
+                list of tools, what they do, how to use them, and keybinds
+                
+              </p>
+              <h2>How it works</h2>
+
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
