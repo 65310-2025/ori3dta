2072becb5650b88c051a35316ecd6662ad3947da
dlee888 <dlee888@mit.edu>
2025-05-07 14:00:50 -0400

    reorg docs

--- client/src/components/pages/Docs.tsx
+++ client/src/components/pages/Docs.tsx
@@ -9,25 +9,18 @@ const Docs: React.FC = () => {
       <Navbar />
       <div className="Home">
         <div className="Docs">
-          <div className="Wrapper">
-            <div className="Page">
-              <h1>Documentation</h1>
-              <h2>Overview</h2>
-              <p>
-                who what why how. core features
-              </p>
-              <h2>Getting started</h2>
-              <p>
-                make a new design, basic tools
-              </p>
-              <h2>Tools reference</h2>
-              <p>
-                list of tools, what they do, how to use them, and keybinds
-                
-              </p>
-              <h2>How it works</h2>
-
-            </div>
+          <div className="Docs-sidebar">
+            <p>TODO: table of contents/sidebar here</p>
+          </div>
+          <div className="Docs-content">
+            <h1>Documentation</h1>
+            <h2>Overview</h2>
+            <p>who what why how. core features</p>
+            <h2>Getting started</h2>
+            <p>make a new design, basic tools</p>
+            <h2>Tools reference</h2>
+            <p>list of tools, what they do, how to use them, and keybinds</p>
+            <h2>How it works</h2>
           </div>
         </div>
       </div>
