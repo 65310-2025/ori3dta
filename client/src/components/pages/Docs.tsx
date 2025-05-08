61cd7858aa42ac0ba7f3ebd488d8a53104a69be0
dlee888 <dlee888@mit.edu>
2025-05-08 09:28:19 -0400

    format stuff

--- client/src/components/pages/Docs.tsx
+++ client/src/components/pages/Docs.tsx
@@ -18,7 +18,6 @@ const Docs: React.FC = () => {
       <div className="Home">
         <div className="Docs">
           <div className="Docs-sidebar">
-            <p>TODO: style table of contents/sidebar here</p>
             <h2>Table of contents</h2>
             <a href="#docs">Documentation</a>
             <a href="#overview">Overview</a>
@@ -29,13 +28,29 @@ const Docs: React.FC = () => {
             <h1 id="docs">Documentation</h1>
             <h2 id="overview">Overview</h2>
             <p>
-              Ori3Dita is a web-based crease pattern software for origami design. The core feature, aside from being web-based, is the ability to draw creases with specified non-flat fold angles. This allows designers to create and accurately document models with intentional 3d structures. Additionally, Ori3Dita introduces a novel algorithm for computing layer ordering and detecting self intersection in non-flat crease patterns.
-              
-              <br /><br />
-              Ori3Dita was built by Brandon Wong, David Lee, Kenny Zhang, and Abby Chou as their final project for Professor Erik Demaine's 6.5310 "Geometric folding algorithms" class at MIT. The project is currently in its "minimum working prototype" phase, but is intended to be further developed into a fully functional crease pattern editor for public use.
-
-              <br /><br />
-              To report a bug, request a feature, or make a pull request, all source code is available at the following Github repository: <a href="https://github.com/65310-2025/ori3dta">https://github.com/65310-2025/ori3dta</a>.              
+              Ori3Dita is a web-based crease pattern software for origami
+              design. The core feature, aside from being web-based, is the
+              ability to draw creases with specified non-flat fold angles. This
+              allows designers to create and accurately document models with
+              intentional 3d structures. Additionally, Ori3Dita introduces a
+              novel algorithm for computing layer ordering and detecting self
+              intersection in non-flat crease patterns.
+              <br />
+              <br />
+              Ori3Dita was built by Brandon Wong, David Lee, Kenny Zhang, and
+              Abby Chou as their final project for Professor Erik Demaine's
+              6.5310 "Geometric folding algorithms" class at MIT. The project is
+              currently in its "minimum working prototype" phase, but is
+              intended to be further developed into a fully functional crease
+              pattern editor for public use.
+              <br />
+              <br />
+              To report a bug, request a feature, or make a pull request, all
+              source code is available at the following Github repository:{" "}
+              <a href="https://github.com/65310-2025/ori3dta">
+                https://github.com/65310-2025/ori3dta
+              </a>
+              .
             </p>
             {/* <p>
               Ori3Dita's web hosting is funded by OrigaMIT. If you would like to financially support the project, you can do so here: ___. If you would like to join the community of Ori3Dita users and developers, you can join the discord here: ___
@@ -60,7 +75,11 @@ const Docs: React.FC = () => {
             </p>
             <h2 id="tools">Tools reference</h2>
             <p>
-              Ori3Dita uses keybinds to optimize the drawing speed for the user. The default keybinds are listed below--most keybinds are easily accessible with the left hand of a QWERTY keyboard, so that the user can keep their right hand on the mouse. Custom keybinds will be a feature in the future.
+              Ori3Dita uses keybinds to optimize the drawing speed for the user.
+              The default keybinds are listed below--most keybinds are easily
+              accessible with the left hand of a QWERTY keyboard, so that the
+              user can keep their right hand on the mouse. Custom keybinds will
+              be a feature in the future.
               <br />
               <br />
               <h3>Edge assignment modes</h3>
@@ -96,81 +115,80 @@ const Docs: React.FC = () => {
               <table>
                 <thead>
                   <tr>
-                  <th>Key</th>
-                  <th>Action</th>
+                    <th>Key</th>
+                    <th>Action</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
-                  <td>Space</td>
-                  <td>Draw</td>
+                    <td>Space</td>
+                    <td>Draw</td>
                   </tr>
                   <tr>
-                  <td>W</td>
-                  <td>Delete</td>
+                    <td>W</td>
+                    <td>Delete</td>
                   </tr>
                   <tr>
-                  <td>Q</td>
-                  <td>Select</td>
+                    <td>Q</td>
+                    <td>Select</td>
                   </tr>
                   <tr>
-                  <td>E</td>
-                  <td>Change MV</td>
+                    <td>E</td>
+                    <td>Change MV</td>
                   </tr>
                   <tr>
-                  <td>T</td>
-                  <td>Edit vertex (fix Kawasaki)</td>
+                    <td>T</td>
+                    <td>Edit vertex (fix Kawasaki)</td>
                   </tr>
                   <tr>
-                  <td>G</td>
-                  <td>Edit crease (change fold angle)</td>
+                    <td>G</td>
+                    <td>Edit crease (change fold angle)</td>
                   </tr>
                   <tr>
-                  <td>Scroll</td>
-                  <td>Zoom in/out</td>
+                    <td>Scroll</td>
+                    <td>Zoom in/out</td>
                   </tr>
                   <tr>
-                  <td>Option + Drag</td>
-                  <td>Pan</td>
+                    <td>Option + Drag</td>
+                    <td>Pan</td>
                   </tr>
                 </tbody>
-                </table>
-
+              </table>
               <h3>3D viewer controls</h3>
-                <table>
+              <table>
                 <thead>
                   <tr>
-                  <th>Key</th>
-                  <th>Action</th>
+                    <th>Key</th>
+                    <th>Action</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
-                  <td>B</td>
-                  <td>Refresh X-ray</td>
+                    <td>B</td>
+                    <td>Refresh X-ray</td>
                   </tr>
                   <tr>
-                  <td>-</td>
-                  <td>Render X-ray with previous starting face</td>
+                    <td>-</td>
+                    <td>Render X-ray with previous starting face</td>
                   </tr>
                   <tr>
-                  <td>=</td>
-                  <td>Render X-ray with next starting face</td>
+                    <td>=</td>
+                    <td>Render X-ray with next starting face</td>
                   </tr>
                   <tr>
-                  <td>Left Click</td>
-                  <td>Rotate model</td>
+                    <td>Left Click</td>
+                    <td>Rotate model</td>
                   </tr>
                   <tr>
-                  <td>Scroll</td>
-                  <td>Zoom in/out</td>
+                    <td>Scroll</td>
+                    <td>Zoom in/out</td>
                   </tr>
                   <tr>
-                  <td>Right Click</td>
-                  <td>Pan</td>
+                    <td>Right Click</td>
+                    <td>Pan</td>
                   </tr>
                 </tbody>
-                </table>
+              </table>
               {/* option drag: pan <br /> */}
             </p>
           </div>
