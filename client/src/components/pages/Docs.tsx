3acf6e46482ad047b4e82c522d780e339a2e01cd
dlee888 <dlee888@mit.edu>
2025-05-08 09:26:39 -0400

    write getting started

--- client/src/components/pages/Docs.tsx
+++ client/src/components/pages/Docs.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 
+import { useNavigate } from "react-router-dom";
+
 import Navbar from "../modules/LandingNavbar";
 import "./Docs.css";
 
 const Docs: React.FC = () => {
+  const navigate = useNavigate();
+
+  const makeNavigate = (path: string) => () => {
+    navigate(path);
+  };
+
   return (
     <div>
       <Navbar />
@@ -22,7 +30,23 @@ const Docs: React.FC = () => {
             <h2 id="overview">Overview</h2>
             <p>who what why how. core features</p>
             <h2 id="getstarted">Getting started</h2>
-            <p>make a new design, basic tools</p>
+            <p>
+              To get started, login with a Google acount{" "}
+              <a onClick={makeNavigate("/login")}>here</a>
+            </p>
+            <p>
+              Once logged in, you can see your designs in the{" "}
+              <a onClick={makeNavigate("/library")}>Library</a> tab, and you can
+              create new designs by clicking the &quot;New Design&quot; button.
+              Optionally, the New Design button also supports importing existing
+              crease patterns. These must be uploaded as a <code>.fold</code>{" "}
+              file.
+            </p>
+            <p>
+              From the Library tab, simply click on a design to open it in the
+              editor. From there, you can edit the crease pattern and see a live
+              3D rendered preview of the folded state.
+            </p>
             <h2 id="tools">Tools reference</h2>
             <p>
               list of tools, what they do, how to use them, and keybinds
